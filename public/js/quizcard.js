c155511b04b0d201455c466e7afa54f9e52481b5
josephbetbeze <josephbetbeze@gmail.com>
2021-12-20 13:27:22 -0500

    Added jquery to show modal and ajax to make the "create" request for quizcards. Updated link in the app.blade.php template to the correct quizcard.js file.

---
+++ public/js/quizcard.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($){
+
+    //----- Open model CREATE -----//
+    jQuery('#btn-add').click(function () {
+        jQuery('#btn-save').val("add");
+        jQuery('#myForm').trigger("reset");
+        jQuery('#formModal').modal('show');
+    });
+
+    // CREATE
+    $("#btn-save").click(function (e) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        e.preventDefault();
+        var formData = {
+            title: jQuery('#title').val(),
+            description: jQuery('#description').val(),
+        };
+        var state = jQuery('#btn-save').val();
+        var type = "POST";
+        var quizcard_id = jQuery('#quizcard_id').val();
+        var ajaxurl = 'quizcard';
+        $.ajax({
+            type: type,
+            url: ajaxurl,
+            data: formData,
+            dataType: 'json',
+            success: function (data) {
+                var quizcard = '<tr id="quizcard' + data.id + '"><td>' + data.id + '</td><td>' + data.title + '</td><td>' + data.description + '</td>';
+                if (state == "add") {
+                    jQuery('#quizcard-list').append(quizcard);
+                } else {
+                    jQuery("#quizcard" + quizcard_id).replaceWith(quizcard);
+                }
+                jQuery('#myForm').trigger("reset");
+                jQuery('#formModal').modal('hide')
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
+    });
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/todo.js') }}" defer></script>
+<script src="{{ asset('js/quizcard.js') }}" defer></script>
 
 </body>
 
