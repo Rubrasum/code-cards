50710758f725cfaff8f0e01858afb527b5df8273
josephbetbeze <josephbetbeze@gmail.com>
2022-01-03 16:29:17 -0500

    QuizCard model - Updated code per convention, added @method declaration(?) to quiet phpstorm warning

--- app/Models/QuizCard.php
+++ app/Models/QuizCard.php
@@ -5,10 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $data)
+ */
 class QuizCard extends Model
 {
     use HasFactory;
     public $timestamps = false;
 
-    protected $fillable = ['user_id','is_archived','group','parent','type','data_string_1','data_string_2'];
+    protected $fillable = [
+        'user_id',
+        'is_archived',
+        'group',
+        'parent',
+        'type',
+        'data_string_1',
+        'data_string_2'
+    ];
 }
