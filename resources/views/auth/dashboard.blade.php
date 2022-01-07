97fc9c78c8cb26d73d54a67fa902eab0ee29f60d
josephbetbeze <60015291+Viridiris@users.noreply.github.com>
2022-01-07 03:23:25 -0500

    Updated dashboard view to a single column grid.

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -7,11 +7,15 @@
     #https://tailwindcomponents.com/component/login-page-1-light-mode
     -->
     <div class="container ">
-        <section class="flex justify-center items-center h-screen">
-            <div class="max-w-md w-full bg-white rounded p-6 space-y-4">
-                <div class="mb-4">
+        <section class="grid items-center grid-cols-1 gap-4 flex justify-center w-100">
+            <div  class="row-span-1 text-center">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="row-span-2 grid-cols-1 w-full bg-white rounded p-6 space-y-2 grid gap-4">
+                <div class="mb-4 row-span-1">
                     <p class="text-site_secondary_color text-3xl font-bold">You are Logged In</p>
                 </div>
+
             </div>
         </section>
     </div>
