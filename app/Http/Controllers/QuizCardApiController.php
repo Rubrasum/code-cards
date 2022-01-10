d803739245276306a0bb78d5c1e1d655874c8d37
josephbetbeze <60015291+Viridiris@users.noreply.github.com>
2022-01-10 04:42:36 -0500

    Converted qcApiController into job dispatch.

--- app/Http/Controllers/QuizCardApiController.php
+++ app/Http/Controllers/QuizCardApiController.php
@@ -39,7 +39,7 @@ class QuizCardApiController extends Controller
     public function store(Request $request)
     {
         // Validate Form Data.
-        // Need to make sure this returns failure. Might do it automatically.
+        // Need to make sure this returns failure. It might happen automatically
         $data = $request->validate([
             'deck' => 'required|max:80',
             'parent' => 'required|max:80',
@@ -47,12 +47,10 @@ class QuizCardApiController extends Controller
             'data_string_1' => 'required|max:510',
             'data_string_2' => 'required|max:510',
         ]);
-        // Create the quizcard object.
-        $quizcard = QuizCard::create($data);
-        // Dispatch the Store QuizCard Job.
-        QuizCardStore::dispatch($quizcard)->onQueue('store');
-        // Send the id back so that the frontend knows which channel to listen to.
-        return $quizcard->id;
+        // Create quizcard object
+        $quizcard = new QuizCard($data);
+        // Dispatch job to save Quizcard, and return result to notify front-end.
+        return QuizCardStore::dispatch($quizcard)->onQueue('store');
     }
 
     /**
